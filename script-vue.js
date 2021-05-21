f5c3cd2a71c5e06b92fe9f74179f441a01327241
Artem <220220220220@rambler.ru>
2021-05-21 19:12:39 +0300

    Нужно разбираться....

--- script-vue.js
+++ script-vue.js
@@ -28,12 +28,63 @@ const makeGETRequest = (url, callback) => {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    el: '#app',
-    data: {
-      goods: [],
-      query: '',
-      basketName: 'Добавить в корзину'
+  Vue.component('search', {
+    template: `<input class="header-input" placeholder="Найти товар" v-model='query' type="text" />`,
+    props: {
+      query: {
+        type: String
+      }
+    },
+    computed: {
+      filteredGoods() {
+        return this.goods.filter((product) => (new RegExp(this.query, 'i')).test(product.title));
+      }
+    },
+    methods: {
+      fetchData() {
+        makeGETRequest(`${baseUrl}${getListUrl}`, (data) => {
+          data = JSON.parse(data);
+          this.goods = this.transformData(data);
+        });
+      },
+      transformData(list) {
+        return list.map((item) => ({
+          title: item.product_name,
+          price: item.price,
+          id: item.product_name
+        }));
+      }
+    },
+    created() {
+      this.fetchData();
+    },
+  })
+
+  Vue.component('GoodsList', {
+    template:
+      `<div>
+        <div v-for="item in filteredGoods" class="goods-list_item">
+            <img />
+            <span class="heading">{{ item.title }}</span>
+            <span class="price">{{ item.price }}</span>
+            <a class="button" href='#'>{{ basketName }}</a>
+          </div>
+        </div>`,
+    props: {
+      goods: {
+        type: Array,
+        default: () => []
+      },
+      should: {
+        type: Boolean
+      },
+
+    },
+    data() {
+      return {
+        query: '',
+        basketName: 'Добавить в корзину'
+      };
     },
     methods: {
       fetchData() {
@@ -59,6 +110,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     created() {
       this.fetchData();
+    },
+    watch: {
+      should: (a, b) => { console.log("wather", a, b) }
+      ,
     }
+  });
+  const app = new Vue({
+    el: '#app',
+    data: {
+      should: false,
+      goodsList: [
+        { title: 'Shirt', price: 150 },
+        { title: 'Socks', price: 50 },
+        { title: 'Jacket', price: 350 },
+        { title: 'Shoes', price: 250 },
+      ],
+    },
+    methods: {
+      handleLoadClick() {
+        this.should = true
+      },
+    },
   });
 });
